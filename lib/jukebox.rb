4425600b21f38e0395ec05845dbdfe1c0d1a8155
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-14 18:49:30 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  if input < songs.length
+  if input.is_a? Integer and input < songs.length
 
       puts "#{songs[input]}"
     #else
