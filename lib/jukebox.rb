2555b37cca0e051192cc0ff69ebdb58f3a0289bc
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-14 18:38:19 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -32,9 +32,7 @@ def play(songs)
     else
       puts "Invalid input, please try again"
     end
-  else
-    puts "Invalid input, please try again"
-  end
+
 end
 def exit_jukebox
   puts "Goodbye"
