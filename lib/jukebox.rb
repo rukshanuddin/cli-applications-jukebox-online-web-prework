c9f7e115a1440731549658d0b80c852f4e046a8e
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-10-14 18:59:28 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -20,7 +20,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   input = gets.strip
-  puts "Playing #{input}"
+  puts "Playing #{songs[input]}"
 end
 def exit_jukebox
   puts "Goodbye"
